5b1cb8b9360c5004246abc90968b80386d0135c6
Vincent <Vincent@Vincents-Mac.local>
2017-06-16 22:59:43 -0700

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,21 +8,23 @@ console.log("User flipped " + cardThree);*/
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
-	}
-	else {
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
